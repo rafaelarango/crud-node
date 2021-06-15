ffb9d3685d9e3aa460112d0456e60ad778aaea61
rafaelarango <arangoramirezrafael@gmail.com>
2021-06-15 18:47:46 -0500

    Configuramos el servisor

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "127.0.0.1"
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
